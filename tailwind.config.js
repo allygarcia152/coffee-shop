3c969140fbccc4cf4540c401daa5ffcc9a4024c9
AllyGarcia <allygarcia152@gmail.com>
2023-02-27 16:36:59 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const autoprefixer = require('autoprefixer');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+}
